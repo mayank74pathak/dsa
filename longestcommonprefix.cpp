2fddae37b295ec53508b15c22b312da3165c73fd
mayank74pathak <56240383+mayank74pathak@users.noreply.github.com>
2024-09-07 15:43:21 +0530

    Create longestcommonprefix.cpp

---
+++ longestcommonprefix.cpp
@@ -0,0 +1,14 @@
+string longestCommonPrefix(vector<string>& strs) {
+        string ans="";
+        sort(strs.begin(),strs.end());
+        int n=strs.size();
+        string first=strs[0];
+        string last=strs[n-1];
+        for(int i=0;i<first.size();i++)
+        {
+            if(first[i]!=last[i])
+            return ans;
+            ans+=first[i];
+        }
+    return ans;
+    }
