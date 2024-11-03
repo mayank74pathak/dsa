3f8539957943e10f4146c5e5cbfccb431c81ccaa
mayank74pathak <56240383+mayank74pathak@users.noreply.github.com>
2024-11-03 12:57:51 +0530

    Update graphrepresentation.cpp

--- graphrepresentation.cpp
+++ graphrepresentation.cpp
@@ -1,6 +1,6 @@
 vector<vector<int>> printGraph(int V, vector<pair<int,int>>edges) {
         // Code here
-    vector<vector<int>>adj(V);
+    vector<vector<int>>adj(V);// dont igmore V because it can cause a segmentation fault
      for(auto i:edges)
      {
          adj[i.first].push_back(i.second);
